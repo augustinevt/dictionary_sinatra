efb28c1e0f27e6e4214e2de4a52d095092f00e45
Augustine von Trapp <Augustvontrapp@gmail.com>
2016-07-08 09:21:17 -0700

    add passing tests for basic Definitions class

---
+++ lib/definition.rb
@@ -0,0 +1,9 @@
+class Definition
+
+  attr_accessor :body
+
+  def initialize(options)
+    @body = options[:body]
+  end
+
+end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -1,10 +1,13 @@
-# require('rspec')
-# require('definitions')
-#
-# describe Definitions do
-#
-#   describe 'Word#initialize' do
-#     it 'should '
-#   end
-#
-# end
+require('rspec')
+require('definition')
+
+describe Definition do
+
+  describe 'Word#initialize' do
+    it 'should return value of it\'s body attibute' do
+      new_definition = Definition.new({body: 'A large rock'})
+      expect(new_definition.body).to eq('A large rock')
+    end
+  end
+
+end
